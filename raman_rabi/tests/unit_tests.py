30d0c572222cde1a8d3397b01e4871c54566ca05
Soumya Ghosh <ghoshss93@gmail.com>
2019-04-17 05:56:04 -0400

    Update unit tests

--- raman_rabi/tests/unit_tests.py
+++ raman_rabi/tests/unit_tests.py
@@ -28,13 +28,13 @@ class TestRR_MODEL(TestCase):
     def test_likelihood_zero_for_nonesense(self):
         testfilepath = "testdata/21.07.56_Pulse Experiment_E1 Raman Pol p0 Rabi Rep Readout - -800 MHz, ND0.7 Vel1, Presel 6 (APD2, Win 3)_ID10708_image.txt"
         s = RRDataContainer.RRDataContainer(testfilepath)
-        s_likelihood = rr_model.likelihood_mN1(s, 0, 0, 0, 0, 0, 0, 0, 0)
+        s_likelihood = rr_model.likelihood_mN1(s, 0, 0, 0, 0, 0, 0, 0, 0)[0]
         self.assertTrue(s_likelihood == 0.0)
 
     def test_likelihood_ratio(self):
         testfilepath = "testdata/21.07.56_Pulse Experiment_E1 Raman Pol p0 Rabi Rep Readout - -800 MHz, ND0.7 Vel1, Presel 6 (APD2, Win 3)_ID10708_image.txt"
         s = RRDataContainer.RRDataContainer(testfilepath)
-        s_likelihood = rr_model.likelihood_mN1(s, 0, 40, 6.10, 16.6881, 1/63.8806, 5.01886, -np.pi/8.77273, 1/8.5871)
-        s2_likelihood = rr_model.likelihood_mN1(s, 0, 40, 10*6.10, 16.6881, 1/63.8806, 5.01886, -np.pi/8.77273, 1/8.5871)
+        s_likelihood = rr_model.likelihood_mN1(s, 0, 40, 6.10, 16.6881, 1/63.8806, 5.01886, -np.pi/8.77273, 1/8.5871)[0]
+        s2_likelihood = rr_model.likelihood_mN1(s, 0, 40, 10*6.10, 16.6881, 1/63.8806, 5.01886, -np.pi/8.77273, 1/8.5871)[0]
         print(s_likelihood)
         self.assertTrue(s_likelihood > s2_likelihood)
