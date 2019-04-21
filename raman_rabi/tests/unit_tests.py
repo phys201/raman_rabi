9c31bb275685274c1baa16dc48ef8b6617ffe5ff
JWatsonDaniels <jamelle.wd@gmail.com>
2019-04-20 21:48:31 -0400

    Tidy up unittests

--- raman_rabi/tests/unit_tests.py
+++ raman_rabi/tests/unit_tests.py
@@ -7,34 +7,28 @@ from raman_rabi import rr_io
 from raman_rabi import rr_model
 import numpy as np
 
+testfilename = "21.07.56_Pulse Experiment_E1 Raman Pol p0 Rabi Rep Readout - -800 MHz, ND0.7 Vel1, Presel 6 (APD2, Win 3)_ID10708_image.txt"
+RRData = rr_io.load_data(rr_io.get_example_data_file_path(testfilename))
+
 class TestTesting(TestCase):
     def test_is_string(self):
         s = testing.hello()
         self.assertTrue(isinstance(s, str))
-
+        
 class TestRR_IO(TestCase):
-    def test_is_RRDataContainer(self):
-        testfilepath = "testdata/21.07.56_Pulse Experiment_E1 Raman Pol p0 Rabi Rep Readout - -800 MHz, ND0.7 Vel1, Presel 6 (APD2, Win 3)_ID10708_image.txt"
-        s = rr_io.load_data(testfilepath)
-        self.assertTrue(isinstance(s,RRDataContainer.RRDataContainer))
+    def test_data_io(self):
+        self.assertTrue(isinstance(RRData, RRDataContainer.RRDataContainer))
 
 class TestRRDataContainer(TestCase):
     def test_correct_dimensions(self):
-        testfilepath = "testdata/21.07.56_Pulse Experiment_E1 Raman Pol p0 Rabi Rep Readout - -800 MHz, ND0.7 Vel1, Presel 6 (APD2, Win 3)_ID10708_image.txt"
-        s = RRDataContainer.RRDataContainer(testfilepath)
-        self.assertTrue(s.get_df().shape == (20,161))
+        self.assertTrue(RRData.get_df().shape == (20,161))
 
 class TestRR_MODEL(TestCase):
     def test_likelihood_zero_for_nonesense(self):
-        testfilepath = "testdata/21.07.56_Pulse Experiment_E1 Raman Pol p0 Rabi Rep Readout - -800 MHz, ND0.7 Vel1, Presel 6 (APD2, Win 3)_ID10708_image.txt"
-        s = RRDataContainer.RRDataContainer(testfilepath)
-        s_likelihood = rr_model.likelihood_mN1(s, 0, 0, 0, 0, 0, 0, 0, 0)[0]
+        s_likelihood = rr_model.likelihood_mN1(RRData, 0, 0, 0, 0, 0, 0, 0, 0)[0]
         self.assertTrue(s_likelihood == 0.0)
 
-    def test_likelihood_ratio(self):
-        testfilepath = "testdata/21.07.56_Pulse Experiment_E1 Raman Pol p0 Rabi Rep Readout - -800 MHz, ND0.7 Vel1, Presel 6 (APD2, Win 3)_ID10708_image.txt"
-        s = RRDataContainer.RRDataContainer(testfilepath)
-        s_likelihood = rr_model.likelihood_mN1(s, 0, 40, 6.10, 16.6881, 1/63.8806, 5.01886, -np.pi/8.77273, 1/8.5871)[0]
-        s2_likelihood = rr_model.likelihood_mN1(s, 0, 40, 10*6.10, 16.6881, 1/63.8806, 5.01886, -np.pi/8.77273, 1/8.5871)[0]
-        print(s_likelihood)
+    def test_likelihood_ratio(self):        
+        s_likelihood = rr_model.likelihood_mN1(RRData, 0, 40, 6.10, 16.6881, 1/63.8806, 5.01886, -np.pi/8.77273, 1/8.5871)[0]
+        s2_likelihood = rr_model.likelihood_mN1(RRData, 0, 40, 10*6.10, 16.6881, 1/63.8806, 5.01886, -np.pi/8.77273, 1/8.5871)[0]
         self.assertTrue(s_likelihood > s2_likelihood)
