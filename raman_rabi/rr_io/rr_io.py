a5785891e854bd3e5d8cffe8dd18adb652ca0c65
JWatsonDaniels <jamelle.wd@gmail.com>
2019-04-20 21:46:20 -0400

    Added get_example_data_file_path function to help users access your example data from outside the package.

--- raman_rabi/rr_io/rr_io.py
+++ raman_rabi/rr_io/rr_io.py
@@ -9,3 +9,16 @@ path to the data file from the working directory
 def load_data(data_file):
     loaded_data = RRDataContainer(data_file)
     return loaded_data
+
+def get_example_data_file_path(filename, data_dir='testdata'):
+    # __file__ is the location of the source file currently in use (so
+    # in this case io.py). We can use it as base path to construct
+    # other paths from that should end up correct on other machines or
+    # when the package is installed
+    start = os.path.abspath(__file__)
+    start_dir = os.path.dirname(start)
+    # Go up two directories
+    up_dir1 = os.path.split(start_dir)[0]
+    up_dir2 = os.path.split(up_dir1)[0]
+    data_dir = os.path.join(up_dir2, data_dir)
+    return os.path.join(up_dir2, data_dir, filename)
