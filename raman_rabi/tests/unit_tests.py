2a126af597dfdf24b858862ce19f4f20d1e42e37
Soumya Ghosh <ghoshss93@gmail.com>
2019-04-24 09:51:00 -0400

    Add unit test for MCMC parameter estimation

--- raman_rabi/tests/unit_tests.py
+++ raman_rabi/tests/unit_tests.py
@@ -5,6 +5,7 @@ from raman_rabi import testing
 from raman_rabi import RRDataContainer
 from raman_rabi import rr_io
 from raman_rabi import rr_model
+import pandas as pd
 import numpy as np
 
 testfilename = "21.07.56_Pulse Experiment_E1 Raman Pol p0 Rabi Rep Readout - -800 MHz, ND0.7 Vel1, Presel 6 (APD2, Win 3)_ID10708_image.txt"
@@ -32,3 +33,29 @@ class TestRR_MODEL(TestCase):
         s_likelihood = rr_model.likelihood_mN1(RRData, 0, 40, 6.10, 16.6881, 1/63.8806, 5.01886, -np.pi/8.77273, 1/8.5871)[0]
         s2_likelihood = rr_model.likelihood_mN1(RRData, 0, 40, 10*6.10, 16.6881, 1/63.8806, 5.01886, -np.pi/8.77273, 1/8.5871)[0]
         self.assertTrue(s_likelihood > s2_likelihood)
+
+class TestParameterEstimation(TestCase):
+    def test_parameter_estimation(self):
+        # previously estimated parameters:
+        theta = np.array([6.10, 16.6881, 1/63.8806, 5.01886, -np.pi/8.77273, 1/8.5871])
+
+        # generate some data
+        test_data = rr_model.generate_test_data(theta, 161, 500, 0, 40)
+
+        # run MCMC on the test data and see if it's pretty close to the original theta
+        guesses = theta
+        numdim = len(guesses)
+        numwalkers = 100 
+        numsteps = 500
+        test_samples = rr_model.Walkers(test_data, guesses, 0, 40, False, dataN=10, scale_factor=100*100, nwalkers=numwalkers, nsteps=numsteps)
+        burn_in_time = 100
+        samples = test_samples.chain[:,burn_in_time:,:]
+        traces = samples.reshape(-1, numdim).T
+        parameter_samples = pd.DataFrame({'BG': traces[0], 'Ap': traces[1], 'Gammap': traces[2], 'Ah': traces[3], 'Omegah': traces[4], 'Gammadeph': traces[5]})
+        MAP = parameter_samples.quantile([0.50], axis=0)
+        self.assertTrue(abs((MAP['BG'].values[0]-guesses[0])/guesses[0]) < 0.1)
+        self.assertTrue(abs((MAP['Ap'].values[0]-guesses[1])/guesses[1]) < 0.1)
+        self.assertTrue(abs((MAP['Gammap'].values[0]-guesses[2])/guesses[2]) < 0.1)
+        self.assertTrue(abs((MAP['Ah'].values[0]-guesses[3])/guesses[3]) < 0.1)
+        self.assertTrue(abs((MAP['Omegah'].values[0]-guesses[4])/guesses[4]) < 0.1)
+        self.assertTrue(abs((MAP['Gammadeph'].values[0]-guesses[5])/guesses[5]) < 0.1)
