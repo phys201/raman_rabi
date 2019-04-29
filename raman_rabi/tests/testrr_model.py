e39a321ec9cfcea05dc1ba0a07e09e52f1eeb807
Soumya Ghosh <ghoshss93@gmail.com>
2019-04-29 10:25:41 -0400

    Add tests for all functions in model.py to resolve #18

--- raman_rabi/tests/testrr_model.py
+++ raman_rabi/tests/testrr_model.py
@@ -16,6 +16,14 @@ class TestRR_MODEL(TestCase):
         s_likelihood = rr_model.likelihood_mN1(RRData, 0, 0, 0, 0, 0, 0, 0, 0)[0]
         self.assertTrue(s_likelihood == 0.0)
 
+    def test_likelihood_mN1(self):
+        time_min = 0
+        time_max = 40
+        theta = np.array([6.10, 16.6881, 1/63.8806, 5.01886, -np.pi/8.77273, 1/8.5871])
+        likelihood = rr_model.likelihood_mN1(RRData,time_min,time_max,*theta)[0]
+        self.assertTrue(likelihood == 3.2035056889734263e-74)
+
+
     def test_likelihood_ratio(self):
         s_likelihood = rr_model.likelihood_mN1(RRData, 0, 40, 6.10, 16.6881, 1/63.8806, 5.01886, -np.pi/8.77273, 1/8.5871)[0]
         s2_likelihood = rr_model.likelihood_mN1(RRData, 0, 40, 10*6.10, 16.6881, 1/63.8806, 5.01886, -np.pi/8.77273, 1/8.5871)[0]
@@ -33,6 +41,80 @@ class TestRR_MODEL(TestCase):
         loglikelihood = rr_model.unbinned_loglikelihood_mN1(bad_guess,
                 test_data, 0, 40, False)
         self.assertTrue(np.isnan(loglikelihood))
+
+    def test_unbinned_loglikelihood_mN1(self):
+        theta = np.array([6.10, 16.6881, 1/63.8806, 5.01886, -np.pi/8.77273, 1/8.5871])
+        time_min = 0
+        time_max = 40
+        fromcsv = True
+
+        likelihood = rr_model.unbinned_loglikelihood_mN1(theta,RRData,time_min,time_max,fromcsv)
+        self.assertTrue(np.round(likelihood,2) == -9253.76)
+
+    def test_laserskew_unbinned_loglikelihood_mN1(self):
+        theta = np.array([6.10, 16.6881, 1/63.8806, 5.01886, -np.pi/8.77273, 1/8.5871])
+        data_length = len(RRData.get_df())
+        theta = np.concatenate((theta,np.ones(data_length)),axis=0)
+        time_min = 0
+        time_max = 40
+        fromcsv = True
+        likelihood = rr_model.laserskew_unbinned_loglikelihood_mN1(theta,RRData,
+            time_min,time_max,fromcsv)
+        self.assertTrue(np.round(likelihood,2) == -9253.76)
+
+    def test_generate_test_data(self):
+        theta = np.array([6.10, 16.6881, 1/63.8806, 5.01886, -np.pi/8.77273, 1/8.5871])
+        timesteps = 5
+        samples = 5
+        time_min = 0
+        time_max = 40
+        # set the seed for reproducibility
+        np.random.seed(0)
+        test_data = rr_model.generate_test_data(theta,timesteps,samples,time_min,time_max)
+        correct_values = np.array([[ 0.0371,  0.0207,  0.0228,  0.0256,  0.0222],
+               [ 0.0227,  0.0231,  0.018 ,  0.0161,  0.0166],
+               [ 0.0286,  0.0253,  0.0219,  0.017 ,  0.0167],
+               [ 0.0296,  0.0255,  0.0177,  0.0178,  0.0117],
+               [ 0.0143,  0.0218,  0.0223,  0.0135,  0.0238]])
+        self.assertTrue(np.all(np.round(test_data.values,4) == correct_values))
+
+    def test_log_prior(self):
+        theta = np.array([6.10, 16.6881, 1/63.8806, 5.01886, -np.pi/8.77273, 1/8.5871])
+        logprior = rr_model.log_prior(theta)
+        self.assertTrue(logprior == 0)
+
+    def test_log_posterior(self):
+        theta = np.array([6.10, 16.6881, 1/63.8806, 5.01886, -np.pi/8.77273, 1/8.5871])
+        time_min = 0
+        time_max = 40
+        fromcsv = True
+
+        logposterior = rr_model.log_posterior(theta,RRData,time_min,time_max,fromcsv)
+        self.assertTrue(np.round(logposterior,2) == -9253.76)
+
+
+
+    def test_ideal_model(self):
+        nsteps = 5
+        time_min = 0
+        time_max = 40
+        theta = np.array([6.10, 16.6881, 1/63.8806, 5.01886, -np.pi/8.77273, 1/8.5871])
+        time, mu = rr_model.ideal_model(nsteps,time_min,time_max,*theta)
+        self.assertTrue(np.all(time == np.array([0.,10.,20.,30.,40.])))
+        self.assertTrue(np.all(np.round(mu,2) == np.array([27.81,18.95,18.61,16.5,15.01])))
+
+    def test_laserskew_log_posterior(self):
+        theta = np.array([6.10, 16.6881, 1/63.8806, 5.01886, -np.pi/8.77273, 1/8.5871])
+        time_min = 0
+        time_max = 40
+        fromcsv = True
+        data_length = len(RRData.get_df())
+        theta = np.concatenate((theta,np.ones(data_length)),axis=0)
+        logposterior = rr_model.laserskew_log_posterior(theta,RRData,time_min,
+                time_max,fromcsv)
+        self.assertTrue(np.round(logposterior,2) == -9253.76)
+
+
 
     def test_parameter_estimation(self):
         # previously estimated parameters:
@@ -66,7 +148,7 @@ class TestRR_MODEL(TestCase):
         theta = np.concatenate( (np.array([6.10, 16.6881, 
                                         1/63.8806, 5.01886, 
                                         -np.pi/8.77273, 1/8.5871]), 
-                                    np.zeros(data_length)+1), axis=0)
+                                    np.ones(data_length)), axis=0)
 
         # generate some data
         test_data = rr_model.generate_test_data(theta[0:6], 161, 
